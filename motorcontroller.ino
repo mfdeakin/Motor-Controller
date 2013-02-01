8a1cecc069a31922abd9bc8935530a0ef772f406
Michael Deakin <Michael.F.Deakin@live.mercer.edu>
2013-02-01 17:35:18 -0500

    Cleaner code, queries motor controller succesfully

--- motorcontroller.ino
+++ motorcontroller.ino
@@ -16,36 +16,59 @@ void setup()
 
 void loop()
 {
-  if(Serial.available() > 0) {
-    byte in = Serial.read();
-    Serial.print("Recieved from S: ");
-    Serial.println(in, DEC);
-  }
   if(Serial1.available() > 0) {
-    byte in = Serial1.read();
-    Serial.print("Recieved from S1: ");
-    Serial.println(in, DEC);
-  }
-  if(Serial2.available() > 0) {
-    byte in = Serial2.read();
-    Serial.print("Recieved from S2: ");
-    Serial.println(in, DEC);
-  }
-  if(Serial3.available() > 0) {
-    byte in = Serial3.read();
-    Serial.print("Recieved from S3: ");
-    Serial.println(in, DEC);
+    while(Serial1.available() > 0) {
+      char in = Serial1.read();
+      in &= 0x7F;
+      Serial.print(in);
+    }
+    Serial.println("");
   }
   static int state = 0;
-//  Serial.write('a');
   if(state) {
     writeString("!A7F\r\n");
   }
   else {
     writeString("!A40\r\n");
   }
-  delay(500);
+  delay(100);
+  readOutput();
+  writeString("?a\r\n");
+  delay(100);
+  readOutput();
+  writeString("?v\r\n");
+  delay(100);
+  readOutput();
+  writeString("?e\r\n");
+  delay(200);
+  readOutput();
   state = !state;
+}
+
+/* Black Magic */
+void startTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) {
+        pmc_set_writeprotect(false);
+        pmc_enable_periph_clk((uint32_t)irq);
+        TC_Configure(tc, channel, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4);
+        uint32_t rc = VARIANT_MCK/128/frequency; //128 because we selected TIMER_CLOCK4 above
+        TC_SetRA(tc, channel, rc/2); //50% high, 50% low
+        TC_SetRC(tc, channel, rc);
+        TC_Start(tc, channel);
+        tc->TC_CHANNEL[channel].TC_IER=TC_IER_CPCS;
+        tc->TC_CHANNEL[channel].TC_IDR=~TC_IER_CPCS;
+        NVIC_EnableIRQ(irq);
+}
+
+void readOutput()
+{
+  if(Serial1.available() > 0) {
+    while(Serial1.available() > 0) {
+      char in = Serial1.read();
+      in &= 0x7F;
+      Serial.print(in);
+    }
+    Serial.println("");
+  }
 }
 
 void writeString(char *str)
